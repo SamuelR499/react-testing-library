781d24f6222cc286dd567c8fa16c8067bd7e231d
Samuelr499 <samuelribeiro2@hotmail.com>
2022-06-07 19:18:01 -0300

    Requisito 5 Pokedex.test

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,53 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
+import App from '../App';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const headingPokdex = screen.getByRole('heading',
+      { name: /Encountered pokémons/i, level: 2 });
+    expect(headingPokdex).toBeInTheDocument();
+  });
+  test('Teste se é exibido o próximo pokémon da lista.', () => {
+    renderWithRouter(<App />);
+    const nextPokemonBtn = screen.getByRole('button', { name: /próximo pokémon/i });
+    expect(nextPokemonBtn).toBeInTheDocument();
+    const pokemonsMenosTheFirst = pokemons.slice(1);
+
+    pokemonsMenosTheFirst.forEach((element) => {
+      userEvent.click(nextPokemonBtn);
+      const pokemonName = screen.getByText(element.name);
+      expect(pokemonName).toBeInTheDocument();
+    });
+  });
+  test('Teste se é mostrado apenas um pokémon por vez;', () => {
+    renderWithRouter(<App />);
+    const qttPokemon = screen.getAllByTestId('pokemon-name');
+    expect(qttPokemon.length).toBe(1);
+  });
+  test('Teste se a Pokédex tem os botões de filtro:', () => {
+    renderWithRouter(<App />);
+    const btnNames = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+    const filterBtn = screen.getAllByTestId('pokemon-type-button');
+    const BtnAllpokemon = screen.getByRole('button', { name: /All/i });
+
+    filterBtn.forEach((item, index) => {
+      expect(item.innerHTML).toBe(btnNames[index]);
+      expect(BtnAllpokemon).toBeInTheDocument();
+    });
+  });
+
+  test('Teste se a Pokédex contém um botão para resetar o filtro:', () => {
+    renderWithRouter(<App />);
+    const BtnAllPokemon = screen.getByRole('button', { name: /All/i });
+
+    expect(BtnAllPokemon.innerHTML).toBe('All');
+    userEvent.click(BtnAllPokemon);
+    const pokemonName = screen.getByText(/Pikachu/i);
+    expect(pokemonName).toBeInTheDocument();
+  });
+});
