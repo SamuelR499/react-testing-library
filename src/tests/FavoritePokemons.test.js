364613adc1b05d2df65135b35acf85f2e688501c
Samuelr499 <samuelribeiro2@hotmail.com>
2022-06-07 17:31:12 -0300

    requisito 3 FavoritePokemons.test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import pokemons from '../data';
+
+// console.log(pokemons);
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibido "No favorite pokemon found";', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+  test('Teste se são exibidos todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<FavoritePokemons
+      pokemons={ [pokemons[0]] }
+    />);
+    const pokemonFavorit = screen.getByText(/pikachu/i);
+    expect(pokemonFavorit).toBeInTheDocument();
+  });
+});
